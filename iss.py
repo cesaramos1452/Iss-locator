24eeb5151bedacddf71ddfca5f94660ca81c535d
Cesar Ramos <cesaramos1452@yahoo.com>
2020-12-16 14:43:15 -0800

    The program is complete does evrything that is being asked

--- iss.py
+++ iss.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import requests
 import turtle
+import time
 
 __author__ = 'Cesaramos1452@yahoo.com'
 
@@ -14,9 +15,9 @@ def in_space():
     names = ', '.join([dic.get('name') for dic in res['people']])
     craft = res['people'][0]['craft']
     total_inspace = res['number']
-    summary = (f"""These {total_inspace} men, {names} are all astronauts who are
+    summary = (f"""These {total_inspace} astronauts, {names} are all astronauts who are
     currently in space on board of the {craft} spacecraft!""")
-    return summary
+    print(summary)
 
 
 def current_coords():
@@ -30,28 +31,50 @@ def current_coords():
     return {'lon': float(lon), 'lat': float(lat), 'timestamp': res['timestamp']}
 
 
-s = turtle.Screen()
-s.setup(width=720, height=360)  # BOOM!
-s.bgpic('map.gif')
-s.setworldcoordinates(-180, -90, 180, 90)
-s.addshape('iss.gif')
-
-iss = turtle.Turtle()
-indy = turtle.Turtle()
-
-iss.shape('iss.gif')
-iss.penup()
-iss.goto(current_coords()['lon'], current_coords()['lat'])
-
-indy.penup()
-indy.goto(-86.148003, 39.791000)
-indy.dot(5, 'yellow')
-indy.hideturtle()
-
-turtle.done()
-# def main():
-#     pass
+def over_indy():
+    """Make an api call to get the estimation
+    time of when The ISS will be overhead Indy"""
+    api = 'http://api.open-notify.org/iss-pass.json'
+    payload = {'lat': 39.791000, 'lon': -86.148003}
+    res = requests.get(api, params=payload).json()
+    next_pass = time.ctime(int(res['request']['datetime']))
+    return next_pass
 
 
-# if __name__ == '__main__':
-#     main()
+def draw_iss():
+    """Creates a world map using turtle module
+    that displays current Location of ISS
+    SpaceCraft and display Indianapolis, Indiana!"""
+    # Turtle setup
+    s = turtle.Screen()
+    s.setup(width=720, height=360)
+    s.bgpic('map.gif')
+    s.setworldcoordinates(-180, -90, 180, 90)
+    s.addshape('iss.gif')
+    # Iss Craft turtle and Indy turtle setup
+    iss = turtle.Turtle()
+    indy = turtle.Turtle()
+
+    iss.shape('iss.gif')
+    iss.penup()
+    iss.goto(current_coords()['lon'], current_coords()['lat'])
+
+    indy.penup()
+    indy.goto(-86.148003, 39.791000)
+    indy.dot(5, 'yellow')
+    indy.color('turquoise')
+    indy.write(over_indy(), align='center',
+               font=("Arial", 10, 'normal',
+                     'bold', 'italic', 'underline'))
+    indy.hideturtle()
+
+    turtle.done()
+
+
+def main():
+    in_space()
+    draw_iss()
+
+
+if __name__ == '__main__':
+    main()
